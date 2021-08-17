e3150f32e8a351c355f6ecac4b9515c859114cee
Rafael Ferreira Lopes <galotinhodelta@gmail.com>
2021-08-17 12:56:33 -0300

    Configuração Zuul

--- src/main/java/br/com/integra/zuul/Application.java
+++ src/main/java/br/com/integra/zuul/Application.java
@@ -2,9 +2,11 @@ package br.com.integra.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
