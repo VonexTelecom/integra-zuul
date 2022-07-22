4524b6384c2882caa348f3d4158bbe9b436f8f7b
paragao01 <paragao01@gmail.com>
2022-07-22 11:40:16 -0300

    Ajuste timeout

--- src/main/java/br/com/integra/zuul/Application.java
+++ src/main/java/br/com/integra/zuul/Application.java
@@ -2,11 +2,9 @@ package br.com.integra.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableCircuitBreaker 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
